95b38566895bbbec4acf0f5772d5f75c0c531875
João Vitor Machado de Almeida <1529435@sga.pucminas.br>
2024-04-19 19:53:01 -0300

    ex01

---
+++ main.cs
@@ -0,0 +1,49 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    /*string firstFriend = "Rafael";
+
+    string secondFriend = "junior";
+
+    Console.WriteLine($"O nome de {firstFriend} tem {firstFriend.Length} letras"); //numero de letras na palavra//
+
+    Console.WriteLine($"O nome de {secondFriend} tem{secondFriend.Length} letras");*/
+
+    /*string apresentacao = "      olá      ";
+
+    Console.WriteLine($"{apresentacao}");
+
+    string apresentacaoCorteR = apresentacao.TrimStart();
+
+    Console.WriteLine($"{apresentacaoCorteR}");
+
+    string apresentacaoCorteL = apresentacao.TrimEnd(); //realocar a palavra para o espaço em branco//
+
+    Console.WriteLine($"{apresentacaoCorteL}");*/
+
+    /*string digaOI = "ola";
+
+    Console.WriteLine(digaOI);
+
+    digaOI = digaOI.Replace("ola", "oi"); //trocar a palavra por outra//
+
+    Console.WriteLine(digaOI);
+
+    Console.WriteLine(digaOI.ToUpper()); //deixar em letra maiuscula//
+
+    Console.WriteLine(digaOI.ToLower());*/ //deixar em lera minuscula//
+
+    /*string letraMusica = "eu preciso dizer que eu te amo";
+
+    Console.WriteLine(letraMusica.Contains("eu"));   //localiza na string a palavra selecionada//
+
+    Console.WriteLine(letraMusica.Contains("cu"));*/
+
+    /*string cartaDespedida = "Não é você, sou eu. Adeus";
+
+    Console.WriteLine(cartaDespedida.EndsWith("Adeus"));  //procura na frase a palavra que começa e a palavra que termina//
+
+    Console.WriteLine(cartaDespedida.StartsWith("ola"));*/
+  }
+}
